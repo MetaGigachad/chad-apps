00062ec0815e115830c761e9d075daa15c91107e
yplishan <yaroslav.plishan@gmail.com>
2024-12-06 14:16:30 +0300

    Fix domains

--- training_frontend/src/api/runtime.ts
+++ training_frontend/src/api/runtime.ts
@@ -13,7 +13,7 @@
  */
 
 
-export const BASE_PATH = "https://training.metachad.duckdns.org/api".replace(/\/+$/, "");
+export const BASE_PATH = "https://training.metachad.mooo.com/api".replace(/\/+$/, "");
 
 export interface ConfigurationParameters {
     basePath?: string; // override base path
